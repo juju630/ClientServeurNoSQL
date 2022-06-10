999104c9f8581d7115c2f507f4f1789124b02bb1
Tristan BAUZIL <trbauzil@IUTCLINFA2319.iut.local>
2022-06-10 11:17:17 +0200

    Changement classe podium

--- Licence/src/main/java/clientServeur/Licence/model/Podium.java
+++ Licence/src/main/java/clientServeur/Licence/model/Podium.java
@@ -1,19 +1,41 @@
 package clientServeur.Licence.model;
 
+import clientServeur.Licence.dto.Pilote_Dto;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.experimental.Accessors;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
-@Getter
-@Setter
-@ToString
+import java.util.Date;
+
+@Data
+@Accessors
+@Document(collection = "Podium")
 public class Podium {
-    private String premier;
-    private String deuxieme;
-    private String troisieme;
+    @Id
+    @MongoId
+    private ObjectId id;
 
-    public Podium(String premier, String deuxieme, String troisieme){
+    @DBRef
+    @Field(value = "premier")
+    private Pilote premier;
+
+    @DBRef
+    @Field(value = "deuxieme")
+    private Pilote deuxieme;
+
+    @DBRef
+    @Field(value = "troisieme")
+    private Pilote troisieme;
+
+    public Podium(Pilote premier, Pilote deuxieme, Pilote troisieme){
         this.premier = premier;
         this.deuxieme = deuxieme;
         this.troisieme = troisieme;
